9e957a5da9780796cb3ef9ee273cd9a482daf0c1
Md Iqbal Hossain <97906560+iqbalhossain038@users.noreply.github.com>
2023-01-02 17:13:23 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.for_sub').click(function(){
+        $('.submenu').toggle('first');
+        $('.message2').toggle();
+        $('.message3').toggle();
+    });
+});
+
+$(document).ready(function(){
+    $('.close_b').click(function(){
+        $('.submenu').toggle('first');
+        $('.message2').toggle();
+        $('.message3').toggle();
+    });
+});
+
+$(document).ready(function(){
+    $('.slider').bxSlider({
+        auto: true,
+    });
+    
+});
